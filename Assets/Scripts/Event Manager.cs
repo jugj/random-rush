df9bd4ae3eda18db19c879f542dd3b49bc312094
Jonathan <jugj@users.noreply.github.com>
2026-07-18 09:42:55 +0200

    Änderungen von Jonathan am 2026-07-18 09:42:55

---
+++ Assets/Scripts/Event Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool continueLoop;
+        DateTime decayLimit = DateTime.Now.AddSeconds(-1);
+        do {
+            continueLoop = false;
+            if (FrameCounter.Count > 0 && FrameCounter[0] < decayLimit) {...}
+    }
+}
