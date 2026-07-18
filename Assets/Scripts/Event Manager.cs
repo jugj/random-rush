78f36bf3579479a7cbbaae60fd77ce9317a626e2
karel <jugj@users.noreply.github.com>
2026-07-18 10:48:29 +0200

    Änderungen von karel am 2026-07-18 10:48:29

--- Assets/Scripts/Event Manager.cs
+++ Assets/Scripts/Event Manager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    float timePassed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class EventManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timePassed += Time.deltaTime;
 
+        if(timePassed > 10f)
+        {
+            int rInt = Random.Range(1, 21);
+            Debug.Log("Zahl: " + rInt);
+
+            if(rInt == 1) {
+
+            }
+
+
+            timePassed = 0f;
+        }
     }
 }
